9946c4f3b4b5b3151ec9ccbba04ae4766bbafada
Adriano Barroso <wiermann@broou.com>
2017-11-09 12:23:11 -0200

    arrumado

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  layout "blog_genial", only: [ :show ]
 
   # GET /articles
   # GET /articles.json
